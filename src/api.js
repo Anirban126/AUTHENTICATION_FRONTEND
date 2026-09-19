a3c0653aff9e7e3e335a6f9c3059344800f68508
Anirban Khanra <anirbankhanra01@gmail.com>
2026-09-19 20:49:01 +0530

    url has changed for new backend

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://authentication-backend-lrqs.onrender.com/api/auth'
+const API_URL = 'https://authentication-backend-1-nqni.onrender.com//api/auth'
 
 
 async function request(endpoint, options = {}) {
